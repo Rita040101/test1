ee3d2367b99ce8cdf521014749bdb6214128d2be
Маргарита  Разводовская <margaritarazvodovskaya@MacBook-Pro-Margarita.local>
2024-12-09 23:02:33 +0300

    обновление

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.math.BigDecimal;
+
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private BigDecimal salary;
+    private int age;
+
+    public Employee(String fullName, String position, String email, String phone, BigDecimal salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void printInfo() {
+        System.out.println("Сотрудник: " + fullName + ", Должность: " + position + ", Email: " + email +
+                ", Телефон: " + phone + ", Зарплата: " + salary + ", Возраст: " + age);
+    }
+}
